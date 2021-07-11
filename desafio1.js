a5d8a6b39eaeabfd34eb96d9614f645460aaf806
LAPTOP-8UVVESR3\Guille <memobarahona@gmail.com>
2021-07-11 11:02:09 -0400

    Desafio uno contar palabras en un texto

--- desafio1.js
+++ desafio1.js
@@ -1,1 +1,10 @@
-let text=''
+let text='node node node javascript node javascript node node javascript node node node node javascript javascript'
+
+let objCount={
+   node:0,
+   js:0
+}
+
+objCount.node=text.split(' ').filter(e=>e=='node').length
+objCount.js=text.split(' ').filter(e=>e=='javascript').length
+console.log(objCount)
